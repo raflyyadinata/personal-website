3f0ebde430d806347127b0ca9974d47e6c6a1fa7
raflyyadinata <raflyadinata46@gmail.com>
2024-04-30 15:04:29 +0700

    tes

---
+++ script.js
@@ -0,0 +1,29 @@
+const dynamicText = document.querySelector("h5 span");
+const words = ["Website Developer", "UI/UX Designer","Blogger", "Graphic Designer"];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+
+    if(!isDeleting && charIndex < currentWord.length) {
+        //if true,type the next character type text
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        //if true,type the next character remove previous
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        isDeleting = !isDeleting;
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect ();
+
